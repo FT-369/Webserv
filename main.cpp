c88315871753bacff248b477bbf4dc542b820670
Jiwon-Woo <jwoo@student.42seoul.kr>
2022-04-27 03:25:46 +0900

    :white_check_mark: test: main 및 event 파싱을 확인하는 코드

--- main.cpp
+++ main.cpp
@@ -4,5 +4,24 @@ int main(void)
 {
 	Config config;
 
+	config.parsingConfig(DEFALUT_CONF);
 	config.printConfig();
+
+	std::map<std::string, std::string>::iterator it;
+	std::map<std::string, std::string> main = config.getMainDirective();
+	std::map<std::string, std::string> event = config.getEventDirective();
+
+
+	for (it = main.begin(); it != main.end(); it++)
+	{
+		std::cout << it->first << ": " << it->second << "|" << std::endl;
+	}
+
+	std::cout << "\n\n";
+	
+	for (it = event.begin(); it != event.end(); it++)
+	{
+		std::cout << it->first << ": " << it->second << "|" << std::endl;
+	}
+	
 }
